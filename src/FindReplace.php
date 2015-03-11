c355c70861e03760090b0baf2c74c590cf1e930e
Connor Abdelnoor & Bryan Borgeson <student@epicodus.com>
2015-03-11 16:39:05 -0700

    deplete moral as we realized the function we used was far to complicated for our simple app

--- src/FindReplace.php
+++ src/FindReplace.php
@@ -5,9 +5,17 @@
 
         function replaceWord($sentence, $word_to_replace, $replacement_word)
         {
+            // $sent_lower = strtolower($sentence);
+            // $repl_lower = strtolower($word_to_replace);
+            // $word_lower = strtolower($replacement_word);
+            //
+            // I WanT to kNOW how my CaT is doing today.
+            // lower case everything
+            // check if lower case thing is in array
+            // if true, replace lower case word with HAUNT
 
-            return preg_replace("/".$word_to_replace."/", $replacement_word, $sentence);
-
+            // return preg_replace("/".$word_to_replace."/", $replacement_word, $sentence);
+            return str_replace($word_to_replace, $replacement_word, $sentence);
         }
 
 
