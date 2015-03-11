9d975847c42e98c35673c5b2c01fcbdfe478afe4
Connor Abdelnoor & Bryan Borgeson <student@epicodus.com>
2015-03-11 16:17:58 -0700

    Hack the matrix.

--- src/FindReplace.php
+++ src/FindReplace.php
@@ -2,25 +2,14 @@
 
     class FindReplace
     {
+
         function replaceWord($sentence, $word_to_replace, $replacement_word)
         {
-
-            $split_sentence = explode(" ",$sentence);
-
-            $output = array();
-
-            foreach ($split_sentence as $split) {
-                if ($word_to_replace == $split) {
-                    array_push($output, $replacement_word);
-                } else {
-                    array_push($output, $split);
-                }
-            }
-
-            return implode(" ", $output);
-
+            
+            return preg_replace("/".$word_to_replace."/", $replacement_word, $sentence);
 
         }
+
 
 
     }
--- tests/FindReplaceTest.php
+++ tests/FindReplaceTest.php
@@ -50,6 +50,55 @@
             $this->assertEquals("z b c", $result);
 
         }
+
+        function test_replace_wordInMultiWord()
+        {
+            //Arrange
+            $test_FindReplace = new FindReplace;
+            $sentence = "be he ho";
+            $word_to_replace = "be";
+            $replacement_word = "ho";
+
+            //Act
+            $result = $test_FindReplace->replaceWord($sentence, $word_to_replace, $replacement_word);
+
+            //Assert
+            $this->assertEquals("ho he ho", $result);
+
+        }
+
+        function test_replace_wordInMultiWord_WithPunctuation()
+        {
+            //Arrange
+            $test_FindReplace = new FindReplace;
+            $sentence = "be, he ho.";
+            $word_to_replace = "be";
+            $replacement_word = "ho";
+
+            //Act
+            $result = $test_FindReplace->replaceWord($sentence, $word_to_replace, $replacement_word);
+
+            //Assert
+            $this->assertEquals("ho, he ho.", $result);
+
+        }
+
+        function test_replace_PartialwordInMultiWord_WithPunctuation()
+        {
+            //Arrange
+            $test_FindReplace = new FindReplace;
+            $sentence = "Hello there.  How are you?";
+            $word_to_replace = "llo";
+            $replacement_word = "ck";
+
+            //Act
+            $result = $test_FindReplace->replaceWord($sentence, $word_to_replace, $replacement_word);
+
+            //Assert
+            $this->assertEquals("Heck there.  How are you?", $result);
+
+        }
+
     }
 
 ?>
