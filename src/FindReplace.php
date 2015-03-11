bb1d0ee5d6ad5f0823c23a70214be3780f08be1d
Connor Abdelnoor & Bryan Borgeson <student@epicodus.com>
2015-03-11 16:19:21 -0700

    for realze, check out the preg replace function to replace text in a string. We did, and now our whole app works in one line of code.

--- src/FindReplace.php
+++ src/FindReplace.php
@@ -5,11 +5,10 @@
 
         function replaceWord($sentence, $word_to_replace, $replacement_word)
         {
-            
+
             return preg_replace("/".$word_to_replace."/", $replacement_word, $sentence);
 
         }
-
 
 
     }
